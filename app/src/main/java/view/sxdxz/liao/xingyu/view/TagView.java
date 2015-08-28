b5cc0640674797d18c95157f52beb88ebb3e7097
sxdxz <gdannyliao@gmail.com>
2015-08-28 15:18:48 +0800

    1:add click listener support 2:add margin support

--- app/src/main/java/view/sxdxz/liao/xingyu/view/TagView.java
+++ app/src/main/java/view/sxdxz/liao/xingyu/view/TagView.java
@@ -3,13 +3,17 @@ package view.sxdxz.liao.xingyu.view;
 import android.content.Context;
 import android.content.res.Resources;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+
+import view.sxdxz.liao.xingyu.myapplication.R;
 
 import static android.view.View.MeasureSpec.makeMeasureSpec;
 import static android.view.View.MeasureSpec.EXACTLY;
@@ -104,6 +108,7 @@ public class TagView extends ViewGroup {
         if (childCount == 0) {
             return;
         }
+
         int paddingLeft = getPaddingLeft();
         int paddingRight = getPaddingRight();
         int paddingTop = getPaddingTop();
@@ -125,21 +130,33 @@ public class TagView extends ViewGroup {
 
             for (int i = hasTitle ? 1 : 0, row = hasTitle ? 1 : 0; i < childCount; i++) {
                 View child = getChildAt(i);
-                int childIndex = i;
-                if (hasTitle) {
-                    childIndex--;
-                }
-                int col = childIndex % lineColumn;
-                if (col == 0 && childIndex != 0) {
-                    row++;
-                }
+                if (child.getVisibility() != GONE) {
+                    LayoutParams layoutParams = child.getLayoutParams();
+                    int marginLeft = 0, marginTop = 0, marginRight = 0, marginBottom = 0;
+                    if (layoutParams instanceof MarginLayoutParams) {
+                        MarginLayoutParams marginLP = (MarginLayoutParams) layoutParams;
+                        marginLeft = marginLP.leftMargin;
+                        marginTop = marginLP.topMargin;
+                        marginRight = marginLP.rightMargin;
+                        marginBottom = marginLP.bottomMargin;
+                    }
 
-                int left = col * childWidth + paddingLeft;
-                int right = left + childWidth;
-                int top = row * childHeight + paddingTop;
-                int bottom = top + childHeight;
+                    int childIndex = i;
+                    if (hasTitle) {
+                        childIndex--;
+                    }
+                    int col = childIndex % lineColumn;
+                    if (col == 0 && childIndex != 0) {
+                        row++;
+                    }
 
-                child.layout(left, top, right, bottom);
+                    int left = col * childWidth + paddingLeft + marginLeft;
+                    int right = left + childWidth - marginLeft - marginRight;
+                    int top = row * childHeight + paddingTop + marginTop;
+                    int bottom = top + childHeight - marginTop - marginBottom;
+
+                    child.layout(left, top, right, bottom);
+                }
             }
         } else {
             childHeight = height;
@@ -169,9 +186,11 @@ public class TagView extends ViewGroup {
         TextView titleView = new TextView(getContext());
         titleView.setText(title);
         titleView.setGravity(Gravity.CENTER_VERTICAL);
+        Resources res = getResources();
         int paddingSmall
-                = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8, getResources().getDisplayMetrics());
+                = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8, res.getDisplayMetrics());
         titleView.setPadding(paddingSmall, 0, 0, 0);
+        titleView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
         addView(titleView, 0);
         hasTitle = true;
     }
@@ -194,9 +213,22 @@ public class TagView extends ViewGroup {
         }
         Context context = getContext();
         for (String t : tags) {
-            TextView textView = new TextView(context);
+            TextView textView = new Button(context);
+            textView.setBackgroundResource(R.drawable.selector_tag_button);
+            MarginLayoutParams lp = new MarginLayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+            lp.setMargins(10,10,10,10);
+            textView.setLayoutParams(lp);
             textView.setText(t);
             addView(textView);
         }
+    }
+
+    @Override
+    public void setOnClickListener(OnClickListener l) {
+        for (int i = hasTitle ? 1 : 0, count = getChildCount(); i < count; i++) {
+            View child = getChildAt(i);
+            child.setOnClickListener(l);
+        }
+        super.setOnClickListener(l);
     }
 }
