40310fadc4910cc8cebbcef752d96006588d5673
sxdxz <gdannyliao@gmail.com>
2015-08-30 18:15:19 +0800

    add sample code

--- app/src/main/java/view/sxdxz/liao/xingyu/myapplication/MainActivityFragment.java
+++ app/src/main/java/view/sxdxz/liao/xingyu/myapplication/MainActivityFragment.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import view.sxdxz.liao.xingyu.view.TagView;
+
 /**
  * A placeholder fragment containing a simple view.
  */
@@ -17,6 +19,14 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View layout = inflater.inflate(R.layout.fragment_main, container, false);
+        TagView tagView = (TagView) layout.findViewById(R.id.tagView);
+        tagView.setTags(new String[] {"标签1", "tag2", "test tag", "try", "love", "simple"});
+        return layout;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
