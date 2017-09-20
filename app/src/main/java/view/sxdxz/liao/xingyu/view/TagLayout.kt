88c622b47ddb2b2179e87c39460a5b4b6c5c269f
sxdxz <gdannyliao@gmail.com>
2017-09-20 21:07:27 +0800

    修复不支持margin的问题

--- app/src/main/java/view/sxdxz/liao/xingyu/view/TagLayout.kt
+++ app/src/main/java/view/sxdxz/liao/xingyu/view/TagLayout.kt
@@ -6,7 +6,6 @@ import android.util.TypedValue
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import java.util.*
 
 /**
  *  TagView是一个布局。它的孩子会随着添加顺序从左到右，从上到下依次排列。它的排列方式遵循以下原则
@@ -19,20 +18,20 @@ import java.util.*
  *  当一个元素是改行最后的元素时，可以使用0dp来设置该元素占用剩余空间
  * Created by liaoxingyu on 8/27/15.
  */
-class TagView @JvmOverloads constructor(
+class TagLayout @JvmOverloads constructor(
         context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : ViewGroup(context, attrs, defStyleAttr) {
     private var maxLineColumn = 5
     private var maxHeight: Int = 0
-    private val layoutCaches = mutableMapOf<View, LayoutPosition>()
+    private val childPositions = mutableMapOf<View, ChildPosition>()
 
-    private class LayoutPosition(val view: View) {
+    private class ChildPosition(val view: View) {
         var left = 0
         var right = 0
         var top = 0
         var bottom = 0
         override fun toString(): String =
-                "LayoutPosition(view=${if (view is TextView) view.text else view}, left=$left, right=$right, top=$top, bottom=$bottom)"
+                "ChildPosition(view=${if (view is TextView) view.text else view}, left=$left, right=$right, top=$top, bottom=$bottom)"
     }
 
     init {
@@ -63,33 +62,37 @@ class TagView @JvmOverloads constructor(
 
         fun hasEnoughSpace(childWidth: Int): Boolean = width - usedWidth >= childWidth
 
+        fun setDisplaySize(cp: ChildPosition, child: View, layoutParams: MarginLayoutParams, startX: Int, startY: Int) {
+            cp.left = startX + layoutParams.leftMargin
+            cp.top = startY + layoutParams.topMargin
+            cp.right = cp.left + child.measuredWidth
+            cp.bottom = cp.top + child.measuredHeight
+        }
+        //used width表示图层已经使用的长度，childTotalWidth表示这个子类实际需要的总长（包括外边距）,displaySize表示子类显示需要的长度（不包括外边距）
         for (i in 0 until childCount) {
             val child = getChildAt(i)
             if (child.visibility == View.GONE) continue
+            val lp = child.layoutParams as MarginLayoutParams
             //假设父布局有足够的空间让子view放置
             if (child.layoutParams.width == 0) {
-                child.layoutParams.width = width - usedWidth
+                child.layoutParams.width = width - usedWidth - lp.leftMargin - lp.rightMargin
                 measureChildWithMargins(child, widthMeasureSpec, usedWidth, heightMeasureSpec, 0)
             } else measureChildWithMargins(child, widthMeasureSpec, 0, heightMeasureSpec, 0)
-            val lp = child.layoutParams as MarginLayoutParams
-            val childWidth = child.measuredWidth + lp.leftMargin + lp.rightMargin
-            val childHeight = child.measuredHeight + lp.topMargin + lp.bottomMargin
+            val childTotalWidth = child.measuredWidth + lp.leftMargin + lp.rightMargin
+            val childTotalHeight = child.measuredHeight + lp.topMargin + lp.bottomMargin
 
-            var layoutCache = layoutCaches[child]
-            if (layoutCache == null) {
-                layoutCache = LayoutPosition(child)
-                layoutCaches.put(child, layoutCache)
+            var cp = childPositions[child]
+            if (cp == null) {
+                cp = ChildPosition(child)
+                childPositions.put(child, cp)
             }
 
-            if (hasEnoughSpace(childWidth) && col < maxLineColumn) {
-                layoutCache.left = usedWidth
-                layoutCache.top = lineTop
-                layoutCache.right = layoutCache.left + childWidth
-                layoutCache.bottom = layoutCache.top + childHeight
+            if (hasEnoughSpace(childTotalWidth) && col < maxLineColumn) {
+                setDisplaySize(cp, child, lp, usedWidth, lineTop)
 
-                usedWidth += childWidth
+                usedWidth += childTotalWidth
                 col++
-                if (childHeight > usedHeight) usedHeight = childHeight
+                if (childTotalHeight > usedHeight) usedHeight = childTotalHeight
                 if (usedWidth > containerWidth) containerWidth = usedWidth
             } else {
                 col = 0
@@ -98,18 +101,15 @@ class TagView @JvmOverloads constructor(
                 containerHeight += usedHeight
                 lineTop = containerHeight
 
-                layoutCache.left = usedWidth
-                layoutCache.top = lineTop
-                layoutCache.right = layoutCache.left + childWidth
-                layoutCache.bottom = layoutCache.top + childHeight
+                setDisplaySize(cp, child, lp, usedWidth, lineTop)
 
-                usedWidth = childWidth
-                usedHeight = childHeight
+                usedWidth = childTotalWidth
+                usedHeight = childTotalHeight
                 println("next line")
             }
             println("usedWidth=$usedWidth, usedHeight=$usedHeight")
 
-            println("v=${if (child is TextView) child.text else child},c=$layoutCache")
+            println("c=$cp")
         }
         containerHeight += usedHeight
 
@@ -132,7 +132,7 @@ class TagView @JvmOverloads constructor(
         for (i in 0 until childCount) {
             val child = getChildAt(i)
             if (child.visibility == View.GONE) continue
-            val layoutPosition = layoutCaches[child] ?: continue
+            val layoutPosition = childPositions[child] ?: continue
             child.layout(layoutPosition.left, layoutPosition.top
                     , layoutPosition.right, layoutPosition.bottom)
         }
@@ -159,7 +159,7 @@ class TagView @JvmOverloads constructor(
         }
     }
 
-    fun setTags(tags: ArrayList<String>) {
+    fun setTags(tags: List<String>) {
         for (t in tags) {
             val textView = TextView(context)
             val lp = ViewGroup.MarginLayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
